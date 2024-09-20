020d9b920c8f033894e1114cfcf5e6e335661982
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2024-09-20 20:53:48 +0530

    adding frontend for add course

---
+++ client/src/components/core/Dashboard/AddCourse/RequitmentField.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+
+function RequirementField({ name, label, register, errors, setValue, getValues }) {
+    const [requirement, setRequirement] = useState("");
+    const [requirementList, setRequirementList] = useState([]);
+
+    useEffect(() => {
+        register(name, {
+            required: true,
+            validate: (value) => value.length > 0,
+        });
+    }, [name, register]);
+
+    useEffect(() => {
+        setValue(name, requirementList);
+    }, [requirementList, setValue, name]);
+
+    const handleAddRequirement = () => {
+        if (requirement) {
+            setRequirementList([...requirementList, requirement]);
+            setRequirement("");
+        }
+    };
+
+    const handleRemoveRequirement = (index) => {
+        const updatedRequirementList = [...requirementList];
+        updatedRequirementList.splice(index, 1);
+        setRequirementList(updatedRequirementList);
+    };
+
+    return (
+        <div>
+            <label htmlFor={name}>{label}</label>
+            <div>
+                <input
+                    type="text"
+                    value={requirement}
+                    onChange={(e) => setRequirement(e.target.value)}
+                    className='w-full'
+                />
+                <button
+                    type='button'
+                    onClick={handleAddRequirement}
+                    className='font-semibold text-yellow-50'
+                >
+                    Add
+                </button>
+            </div>
+            {requirementList.length > 0 && (
+                <ul>
+                    {requirementList.map((requirement, index) => (
+                        <li key={index}>
+                            <span>{requirement}</span>
+                            <button
+                                type='button'
+                                onClick={() => handleRemoveRequirement(index)}
+                                className='text-xs text-richblack-300'
+                            >
+                                Clear
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            {errors[name] && (
+                <span>{label} is required</span>
+            )}
+        </div>
+    );
+}
+
+export default RequirementField;
