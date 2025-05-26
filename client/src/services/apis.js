3c8869f178cec159f66c3c8c8d775e8312d6557a
Rohith Varma Datla <r0h17h@RohithVarma-Macbook-Air.local>
2025-05-26 20:52:52 +0530

    updating the base_url

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -1,5 +1,4 @@
-//const BASE_URL = "https://study-notion-backend-0s6t.onrender.com/api/v1"
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://studynotion-edtech-platform-vtyb.onrender.com/api/v1"
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendOtp",
